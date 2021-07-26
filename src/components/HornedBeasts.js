041827483080aee14e70f92e2b7bce1d1740f433
naeemalomari <naeemalomari96@gmail.com>
2021-07-26 17:33:34 +0300

    done with class2

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-class HornedBeasts extends React.Component{
-render(){
-    return(
-        <div>
-            <h2> {this.props.title} </h2>
-            <img 
-            src={this.props.imageUrl}
-            alt={this.props.title}
-            title={this.props.title}
-            />
-            <p> 
-                {this.props.description}
-            </p>
-        </div> 
-    );
-}
+class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            numOfHornedBeast: 0
+        };
+    }
+    increaseVote = () => {
+        this.setState({
+            numOfHornedBeast: this.state.numOfHornedBeast + 1
+        });
+    };
+    render() {
+        return (
+            <Card style={{ width: '18rem', height :"30rem" }}
+                onClick={this.increaseVote}>
+
+                <Card.Img variant="top" src={this.props.image_url} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                    <Button variant="primary">Click for Voting 💟 : {this.state.numOfHornedBeast}</Button>
+                </Card.Body>
+            </Card>
+        );
+    }
 }
 export default HornedBeasts;
